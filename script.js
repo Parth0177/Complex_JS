b4cca705118329786f095467913ad42e6a24ea11
Parth Tiwari <parthtiwari670@gmail.com>
2025-05-25 15:40:37 +0530

    Add content to index.html and create script.js for JSON parsing

---
+++ script.js
@@ -0,0 +1,4 @@
+let jsonRes = '{"fact":"In ancient Egypt, when a family cat died, all family members would shave their eyebrows as a sign of mourning.","length":110}';
+
+let validRes= JSON.parse(jsonRes);
+console.log(validRes.fact);
