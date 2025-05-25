804ab3021f9956ce0a49fb1c4dcb29c953ee6635
Parth Tiwari <parthtiwari670@gmail.com>
2025-05-26 01:02:00 +0530

    Refactor fetch response handling to log fetched data

--- url.js
+++ url.js
@@ -3,7 +3,10 @@ let url="https://catfact.ninja/fact";
 fetch(url)
 .then((response)=>{
   console.log(response);
-  
+  response.json().then((data)=>{
+    console.log("Data fetched successfully:", data);
+    
+  })
 })
 .catch((error)=>{
   console.log("Error fetching data:", error);
